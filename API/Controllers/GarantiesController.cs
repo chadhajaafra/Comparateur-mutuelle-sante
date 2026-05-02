414ab7e19a6f1e26fe28563afa4e0d558ae2ea0e
chadhajaafra <115808954+chadhajaafra@users.noreply.github.com>
2026-05-02 23:21:14 +0100

    Add GrantiesController

---
+++ API/Controllers/GarantiesController.cs
@@ -0,0 +1,20 @@
+﻿using Comparateur.Domain.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Comparateur.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GarantiesController : ControllerBase
+    {
+        private readonly IGarantieRepository _repo;
+
+        public GarantiesController(IGarantieRepository repo) => _repo = repo;
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> GetAll(CancellationToken ct)
+            => Ok(await _repo.GetAllAsync(ct));
+    }
+}
